37f8f4d3bb26274f54bc3efc33e0d8a1b1b10150
Tigran <tigran-a@outlook.com>
2026-09-24 00:22:26 +0400

    fix: validate current position input and retry on invalid coordinates

--- MyChess_Console/Program.cs
+++ MyChess_Console/Program.cs
@@ -7,48 +7,46 @@ Board board = new Board();
 board.FitPiecesOnBoard();
 board.DrawSymbolsOnBoard();
 
-Console.WriteLine("Please enter the current piece position: ");
-string? currentPosition = Console.ReadLine();
-
 Position currPos = new Position();
 
-try
+Console.WriteLine("Please enter the current piece position: ");
+string? currentPosition = Console.ReadLine(); ;
+
+while (true)
 {
-    if (!string.IsNullOrEmpty(currentPosition)) //kisat
+    if (currentPosition is null)
+        return;
+    if (currentPosition.Length == 2)
     {
         int currRow = '8' - currentPosition[1];
         int currCol = (int)currentPosition[0] - 97;
 
-        currPos.Row = currRow;
-        currPos.Col = currCol;
-        //Console.WriteLine($"Row: {currPos.Row}\nCol: {currPos.Col}");
+        if ((currRow >= 0 && currRow <= 7) && (currCol >= 0 && currCol <= 7))
+        {
+            currPos.Row = currRow;
+            currPos.Col = currCol;
+            break;
+        }
     }
-    else
-    {
-        Console.WriteLine("Please enter correct piece position.ex: e3");
-    }
 
-}
-catch (InvalidPositionException exp)
-{
-    Console.WriteLine($"Exception: {exp.Message}");
+    Console.WriteLine("Please enter correct current piece position.ex: e3");
+    currentPosition = Console.ReadLine();
 }
 
+//Console.WriteLine("Please enter the piece new position: ");
+//string? newPosition = Console.ReadLine();
 
-Console.WriteLine("Please enter the piece new position: ");
-string? newPosition = Console.ReadLine();
+//Position newPos = new Position();
+//if (!string.IsNullOrEmpty(newPosition) && newPosition.Length is 2)
+//{
+//    newPos.Row = '8' - newPosition[1];
+//    newPos.Col = (int)newPosition[0] - 97;
 
-Position newPos = new Position();
-if (!string.IsNullOrEmpty(newPosition) && newPosition.Length is 2)
-{
-    newPos.Row = '8' - newPosition[1];
-    newPos.Col = (int)newPosition[0] - 97;
-  
-    //Console.WriteLine($"Row: {newPos.Row}\nCol: {newPos.Col}");
-}
-else
-{
-    Console.WriteLine("Please enter correct position, ex: e2");
-}
+//    //Console.WriteLine($"Row: {newPos.Row}\nCol: {newPos.Col}");
+//}
+//else
+//{
+//    Console.WriteLine("Please enter correct position, ex: e2");
+//}
 
-board.UpdateBoard(currPos, newPos);
+//board.UpdateBoard(currPos, newPos);
